7c23f215eb5ceadf257201e4b57b6909f8fa5d32
Praveshvermaa <praveshdhakd62@gmail.com>
2025-07-03 17:59:56 +0530

    url

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://noteshub-backend-ptnu.onrender.com/api",//https://noteshub-backend-ptnu.onrender.com
+  baseURL: "https://noteshub-server.onrender.com/api",//https://noteshub-backend-ptnu.onrender.com
   withCredentials: true, 
 });
 
